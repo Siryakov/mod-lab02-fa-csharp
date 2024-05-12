4bcef592e10061b5c603bf4df3e299efb262bd46
Kirill Siryakov <121825657+Siryakov@users.noreply.github.com>
2024-05-12 16:12:33 +0300

    Update program.cs

--- fa/program.cs
+++ fa/program.cs
@@ -4,41 +4,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace fans
-{
-    // Класс, представляющий состояние автомата
-    public class State
-    {
-        public string Name;  // Название состояния
-        public Dictionary<char, State> Transitions;  // Переходы из текущего состояния по символам
-        public bool IsAcceptState;  // Флаг, указывающий, является ли состояние финальным (принимающим)
-    }
+ // Класс, реализующий конечный автомат FA1
+ public class FA1
+ {
+     // Определение состояний автомата FA1
+     public static State a = new State()
+     {
+         Name = "a",
+         IsAcceptState = false,
+         Transitions = new Dictionary<char, State>()
+     };
+     public State b = new State()
+     {
+         Name = "b",
+         IsAcceptState = false,
+         Transitions = new Dictionary<char, State>()
+     };
+     public State c = new State()
+     {
+         Name = "c",
+         IsAcceptState = false,
+         Transitions = new Dictionary<char, State>()
+     };
+     public State d = new State()
+     {
+         Name = "d",
+         IsAcceptState = false,
+         Transitions = new Dictionary<char, State>()
+     };
+     public State e = new State()
+     {
+         Name = "e",
+         IsAcceptState = true,
+         Transitions = new Dictionary<char, State>()
+     };
 
+     State InitialState = a;
+     public FA1()
+     {
+         a.Transitions['0'] = b;
+         a.Transitions['1'] = c;
 
-    // Класс, реализующий конечный автомат FA1
-    public class FA1
-    {
-        public static readonly State a = new State { Name = "a", IsAcceptState = false, Transitions = new Dictionary<char, State>() };
-        public static readonly State b = new State { Name = "b", IsAcceptState = true, Transitions = new Dictionary<char, State>() };
-        public static readonly State с = new State { Name = "с", IsAcceptState = true, Transitions = new Dictionary<char, State>() };
+         b.Transitions['0'] = d;
+         b.Transitions['1'] = e;
 
-        public static readonly State InitialState = a;
-        public FA1()
-        {
-            a.Transitions['0'] = b;
-            a.Transitions['1'] = a;
-            b.Transitions['1'] = с;
-            с.Transitions['1'] = с;
-        }
+         c.Transitions['0'] = e;
+         c.Transitions['1'] = c;
 
-        // Метод Run выполняет обработку входной последовательности символов и возвращает результат
-        public bool? Run(IEnumerable<char> s)
-        {
-            State current = InitialState;
-            // Проверяем наличие перехода для всех символов во входной последовательности с использованием LINQ
-            return s.All(c => current.Transitions.TryGetValue(c, out current)) ? current.IsAcceptState : (bool?)null;
-        }
-    }
+         d.Transitions['0'] = d;
+         d.Transitions['1'] = d;
+
+         e.Transitions['0'] = d;
+         e.Transitions['1'] = e;
+     }
+
+     // Метод Run выполняет обработку входной последовательности символов и возвращает результат
+     public bool? Run(IEnumerable<char> s)
+     {
+         State current = InitialState;
+         // Проверяем наличие перехода для всех символов во входной последовательности с использованием LINQ
+         return s.All(c => current.Transitions.TryGetValue(c, out current)) ? current.IsAcceptState : (bool?)null;
+     }
+ }
     // дял классов FA2 и FA3 всё аналогично
     public class FA2
     {
